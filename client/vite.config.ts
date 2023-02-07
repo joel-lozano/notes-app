9ce8687f1b80ef06d611e3c0be3ddefa7a17044c
Joel Lozano <lozano.joel.a@gmail.com>
2023-02-06 19:10:26 -0500

    Added vite config option depending on environment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/notes-app/',
+  base: process.env.NODE_ENV === 'production' ? '/notes-app/' : undefined,
   plugins: [react()],
   server: {
     proxy: {
